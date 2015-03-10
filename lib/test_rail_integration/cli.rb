1aa7a34b949518c85d35c84d056db2073d418ce5
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2015-03-10 19:39:35 +0700

    Added possibility to generate executable file with gem command

--- lib/test_rail_integration/cli.rb
+++ lib/test_rail_integration/cli.rb
@@ -1,13 +1,20 @@
 require 'thor'
-require_relative 'generator/project'
+require 'generator/project'
+require 'generator/project/test_run'
 
 module TestRailIntegration
   class CLI < Thor
-    desc "perform", "Creates project for interaction with TestRail"
 
-    def perform
-      TestRailIntegration::TestTail::Generators::Project.copy_file('run_test_run.rb')
-      TestRailIntegration::TestTail::Generators::Project.copy_file("test_rail_data.yml", "config/data/")
+    desc "prepare", "Creates project for interaction with TestRail"
+    def prepare
+      TestRailIntegration::TestRail::Generators::Project.copy_file("test_rail_data.yml", "config/data/")
     end
+
+    desc "create", "Create test run and executable file"
+    option :args
+    def create
+      TestRail::TestRun.create(args)
+    end
+
   end
 end
