eb152f7848999ba943ddd074b44a620b199021d9
Andrey Tolpeev <andrey.tolpeev@gmail.com>
2015-03-12 17:51:15 +0700

    Fix for teamcity coverage formatter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rspec'
 require 'simplecov'
+require 'simplecov-teamcity-summary/formatter'
 
 SimpleCov.start do
   at_exit do
